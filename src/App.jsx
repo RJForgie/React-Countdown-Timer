fcf20f7a20485c4accd8e8072abd78c74139f29d
Ryan Forgie <ryan.j.forgie@gmail.com>
2018-01-09 20:56:26 +0000

    mvp complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
 import React, { Component } from 'react'
+import Clock from './Clock.jsx'
 import './App.css'
+import { Form, FormControl, Button } from 'react-bootstrap'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: '25 December, 2018',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline})
+  }
+
   render(){
     return (
       <div className='App'>
-        <div className='App-title'>Countdown to 21 December, 2018</div>
-        <div>
-          <div className='Clock-days'>14 Days</div>
-          <div className='Clock-hours'>13 Hours</div>
-          <div className='Clock-minutes'>15 Minutes</div>
-          <div className='Clock-seconds'>15 Seconds</div>
+        <div className='App-title'>
+          Countdown to {this.state.deadline}
         </div>
-        <input placeholder='new date'/>
-        <button>Submit</button>
+          <Clock
+            deadline={this.state.deadline}
+          />
+          <Form inline>
+            <FormControl
+              className="Deadline-Input"
+              placeholder='new date'
+              onChange={event => this.setState({newDeadline: event.target.value})}
+            />
+            <Button onClick={() => this.changeDeadline()}>
+              Submit
+            </Button>
+          </Form>
       </div>
     )
   }
 }
 
-export default App;
+export default App
